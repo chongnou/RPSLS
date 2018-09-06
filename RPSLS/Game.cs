bbeb9c7969ce936ee334878c3019e992dd067f0f
chong <39102393+chongnou@users.noreply.github.com>
2018-09-06 03:20:12 -0500

    added game class

---
+++ RPSLS/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorLizardSpock
+{
+    class Game
+    {
+        public Player playerOne;
+        public Player playerTwo;
+        public double winCondition;
+        public string numberOfPlayers;
+        public double PlayerOneOrPlayerTwoMessageTracker;
+
+        public Game()
+        {
+            winCondition = 2;
+        }
+
+        public void MainMenu()
+        {
+            Console.WriteLine("--Welcome to RPSLS!--");
+            Players();
+            Play();
+        }
+
+        
+    }
+}
