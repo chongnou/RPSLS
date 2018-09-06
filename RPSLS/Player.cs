824ca7bf396a266cd139499c9f1e63411589a1b6
chong <39102393+chongnou@users.noreply.github.com>
2018-09-06 03:27:34 -0500

    added computer class

--- RPSLS/Player.cs
+++ RPSLS/Player.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace RPSLS
 {
-    class Player
+    public abstract class Player
     {
+        public string name;
+        public double score;
+        public string gesture;
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void IncreaseScore()
+        {
+            score++;
+            GetScore();
+        }
+
+        public void GetScore()
+        {
+            Console.WriteLine("The player " + name + " has a score of " + score + ".");
+        }
+
+        public abstract string Gesture();
     }
 }
