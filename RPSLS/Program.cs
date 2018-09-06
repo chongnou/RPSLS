1bcfe14446160281345997e46cf34d4325e29923
chong <39102393+chongnou@users.noreply.github.com>
2018-09-06 03:23:53 -0500

    added player class

--- RPSLS/Program.cs
+++ RPSLS/Program.cs
@@ -10,6 +10,9 @@ namespace RPSLS
     {
         static void Main(string[] args)
         {
+            Game playGame = new Game();
+            playGame.MainMenu();
+            Console.ReadLine();
         }
     }
 }
